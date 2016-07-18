dfc5f58d86b6819a6f587466adcb2d1bfa7f5a2f
Jhoan Manuel Zarria Sinarahua <jhoan@lighthammer.jhoan>
2016-07-18 14:25:11 -0500

    reload

---
+++ application/views/layout.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<title>Proctor - by Creligent </title>
+	<link href="favicon1.html" rel="icon" type="image/x-icon" />
+	<?php include("assets/layouts/css.php"); ?>
+</head>
+
+<body>
+
+	<?php include("assets/layouts/header.php"); ?>
+
+	<div class="container" id="content-container">
+		<div class="content-wrapper">
+			<div class="row">
+				<div class="side-nav-content">
+					<?php include("assets/layouts/menu.php"); ?>
+					<div class="main-content-wrapper">
+						<div class="container-fluid container-padded dash-controls">
+							<div class="row">
+								<div class="col-md-12">
+									<ol class="breadcrumb">
+										<li><a href="#">Home</a></li>
+										<li><a href="#">Library</a></li>
+										<li class="active">Data</li>
+									</ol>
+								</div>
+							</div>
+						</div>
+						<div class="main-content">
+							<section>
+								<div class="container-fluid container-padded">
+									<div class="row">
+										<div class="col-md-12 page-title">
+											<h2>Blank page</h2>
+											<hr>
+										</div>
+									</div>
+								</div>
+								<div class="container-fluid container-padded">
+									<div class="row">
+										<div class="col-md-12">
+											Blank page.
+										</div>
+									</div>
+								</div>
+							</section>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php include("assets/layouts/footer.php"); ?>
+	<?php include("assets/layouts/js.php"); ?>
+</body>
+
+</html>
---
+++ assets/layouts/js.php
@@ -0,0 +1,17 @@
+<script src="<?php echo base_url() ?>assets/js/lib/jquery/jquery-1.10.2.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url() ?>assets/js/lib/bootstrap/bootstrap.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url() ?>assets/js/lib/slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url() ?>assets/js/lib/momentjs/moment.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url() ?>assets/js/lib/daterangepicker/daterangepicker.js" type="text/javascript"></script>
+<script src="<?php echo base_url() ?>assets/js/lib/tabdrop/bootstrap-tabdrop.js" type="text/javascript"></script>
+<script src="<?php echo base_url() ?>assets/js/scripts.min.js" type="text/javascript"></script>
+<script>
+	/*(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','../../../www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-45950338-1', 'creligent.com');
+	ga('send', 'pageview');*/
+
+</script>
