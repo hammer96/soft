ebc6616f951bec4070b19a1a99244c76493106ef
Jhoan Manuel Zarria Sinarahua <jhoan@lighthammer.jhoan>
2016-08-01 13:41:42 -0500

    reload

--- assets/layouts/css.php
+++
@@ -1,7 +0,0 @@
-<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/lib/bootstrap/bootstrap.css" />
-<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/lib/font-awesome/font-awesome.min.css">
-<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/lib/ionicons/ionicons.min.css">
-<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/lib/daterangepicker/daterangepicker-bs3.css" />
-<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/lib/tabdrop/tabdrop.css" />
-<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/styles.css" id="theme-css" />
-<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/lib/datatables/datatables.css">
